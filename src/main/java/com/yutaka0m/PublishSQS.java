93c2b087c3fd23ee1d4aaf55b2b4eafbde3d1d6f
Yutaka MIyamae <yutaka.miyamae@askul.com>
2020-09-25 22:11:13 +0900

    update

--- src/main/java/com/yutaka0m/PublishSQS.java
+++ src/main/java/com/yutaka0m/PublishSQS.java
@@ -10,20 +10,24 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class PublishSQS {
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) throws URISyntaxException, InterruptedException {
         var queueUrl = "http://localhost:9324/queue/test";
 
         SqsClient sqsClient = SqsClient.builder()
             .credentialsProvider(DefaultCredentialsProvider.create())
             .region(Region.AP_NORTHEAST_1)
-            .endpointOverride(new URI("http://localhost:9324")) // ElasticMQコンテナ向けのURL
+            .endpointOverride(new URI(queueUrl)) // ElasticMQコンテナ向けのURL
             .build();
 
-        SendMessageResponse res =
-            sqsClient.sendMessage(
-                SendMessageRequest.builder()
-                    .queueUrl(queueUrl)
-                    .messageBody("Hello world!")
-                    .build());
+        for (int i = 0; i < 20; i++) {
+            SendMessageResponse res =
+                sqsClient.sendMessage(
+                    SendMessageRequest.builder()
+                        .queueUrl(queueUrl)
+                        .messageBody("Hello world!")
+                        .build());
+
+            Thread.sleep(10000);
+        }
     }
 }
